73c3035b413f967458de715ba9d4061d9b6ef893
hamzabro010 <45126025+hamzabro010@users.noreply.github.com>
2018-11-21 19:21:29 +0500

    Add files via upload

---
+++ pp1.py
@@ -0,0 +1,38 @@
+print("Muhammad Hamza Adnan - 18b-017-cs-Sec-'A'")
+print("Assignment # 3")
+print("Practice problem 3.1(a)")
+
+age = eval(input('Enter the given age:'))
+if age > 62:
+    print("You can get your benefits")
+elif age > 62:
+    print("Sorry, you cannot get your benefits")
+
+
+print("Practice problem 3.1(b)")
+
+lst = ('Musial', 'Aaron', 'Williams', 'Gehrig', 'Ruth'),
+if 'Michael' in lst:
+    print('One of the top 5 baseball players, ever!')
+else:
+    print('Not the best of the top 5 baseball players')
+
+
+print("Practice problem 3.1(c)")
+
+hits = int(input('Enter the value of hits :'))
+Shield = int(input('Enter the value of shield :'))
+if 'hits > 10 and shield == 0' in 'hits and shield':
+    print('You are dead')
+else:
+    print('You are safe')
+
+
+print("Practice problem 3.1(d)")
+
+lst = ['north','south','east','west']
+direction = str(input("Enter the direction: "))
+if direction in lst:
+    print("I can escape")
+else:
+    print("I'm in danger")
